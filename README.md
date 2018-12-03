0dfd9ba26116f719121d38364034fe72801ad679
florianakos <florian.akos.szabo@gmail.com>
2018-12-03 15:49:59 +0100

    Init

---
+++ README.md
@@ -0,0 +1,48 @@
+## AWS CloudWatcher Library ##
+
+This is a simple Go library that wraps the "AWS GO SDK" library to provide a way to query AWS Cloudwatch metrics from EC2 instances.
+
+### Example Usage: ###
+
+```go
+fmt.Println(cloudwatcher.GetMetricData("eu-central-1", "i-08b39fb1d301c6cf9"))
+```
+
+### Example output: ###
+
+```json
+{
+  MetricDataResults: [{
+      Id: "id1",
+      Label: "CPUUtilization",
+      StatusCode: "Complete",
+      Timestamps: [
+        2018-12-03 14:39:00 +0000 UTC,
+        2018-12-03 14:34:00 +0000 UTC,
+        2018-12-03 14:29:00 +0000 UTC,
+        2018-12-03 14:24:00 +0000 UTC,
+        2018-12-03 14:19:00 +0000 UTC,
+        2018-12-03 14:14:00 +0000 UTC,
+        2018-12-03 14:09:00 +0000 UTC,
+        2018-12-03 14:04:00 +0000 UTC,
+        2018-12-03 13:59:00 +0000 UTC,
+        2018-12-03 13:54:00 +0000 UTC,
+        2018-12-03 13:49:00 +0000 UTC
+      ],
+      Values: [
+        1.999435028248588,
+        0.0338983050847462,
+        0,
+        0.032786885245900996,
+        0.0338983050847462,
+        0,
+        0.233333333333334,
+        0.0333333333333338,
+        0.6595628415300541,
+        0.0333333333333332,
+        1.240279707326108
+      ]
+    }]
+}
+
+```
---
+++ cloudwatcher.go
@@ -0,0 +1,71 @@
+package cloudwatcher
+
+import (
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/cloudwatch"
+
+    "strconv"
+    "time"
+    "log"
+)
+
+// global variable needed to generate unique request IDs 
+// in case multiple metrics are requested
+var counter int64 = 0
+
+
+// build data structure to request the metricname from the given instance ID 
+func buildMetricDataQuery(metricname, instanceID string) *cloudwatch.MetricDataQuery {
+	counter++
+	return &cloudwatch.MetricDataQuery{
+		Id: aws.String("id" + strconv.FormatInt(counter, 10)),
+		MetricStat: &cloudwatch.MetricStat{
+			Period:     aws.Int64(60),
+			Stat: aws.String("Average"),
+			Metric: &cloudwatch.Metric{
+				MetricName: aws.String(metricname),
+				Dimensions: []*cloudwatch.Dimension{
+					{
+						Name:  aws.String("InstanceId"),
+						Value: aws.String(instanceID),
+					},
+				},
+				Namespace:  aws.String("AWS/EC2"),
+			},
+		},
+	}
+}
+
+
+// get CPU-Utilization for the last hour for the instance in the specified region
+func GetMetricData(region string, instanceID string) *cloudwatch.GetMetricDataOutput {
+	sess, err := session.NewSession(&aws.Config{ Region: aws.String(region)}, )
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	cw := cloudwatch.New(sess)
+
+
+	// prepare request input structure
+	dataInput := &cloudwatch.GetMetricDataInput{
+		StartTime:  aws.Time(time.Now().Add(-60* time.Minute)),
+		EndTime:    aws.Time(time.Now()),
+		MetricDataQueries: []*cloudwatch.MetricDataQuery{
+			buildMetricDataQuery("CPUUtilization", instanceID),
+			//buildMetricDataQuery("NetworkIn", instanceID),
+
+		},
+	}
+
+	// request MetricData in batch
+	dataOutput, err := cw.GetMetricData(dataInput)
+	if err != nil {
+		log.Println("error GetMetricStatistics: ", err)
+		return nil
+	}
+
+	// return results
+	return dataOutput
+}
